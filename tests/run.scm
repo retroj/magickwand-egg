ae5604aa1ac63f9ffaec1ef091070767ce78bfc0
John Foerch <jjfoerch@earthlink.net>
2012-04-26 02:00:56 -0400

    new test: "rose: characteristics"
    tests things like the width and height of the built-in 'rose:' image.

--- tests/run.scm
+++ tests/run.scm
@@ -20,4 +20,14 @@
              (string-contains s "ImageMagick Studio LLC")
              #t)))
 
+(test "rose: characteristics"
+      '("ROSE" "PPM" 46 70 TrueColorType)
+      (let ((w (new-magickwand)))
+        (magick-read-image w "rose:")
+        (list (magick-get-image-filename w)
+              (magick-get-image-format w)
+              (magick-get-image-height w)
+              (magick-get-image-width w)
+              (magick-get-image-type w))))
+
 (magickwand-terminus) ;; why?
