9c28faecfe069209218c490fa93bb735b230fe01
John Foerch <jjfoerch@earthlink.net>
2014-12-08 13:23:40 -0500

    legalese update

--- magickwand-impl.scm
+++ magickwand-impl.scm
@@ -1,4 +1,4 @@
-;; Copyright 2012 John J Foerch. All rights reserved.
+;; Copyright 2012,2014 John J Foerch. All rights reserved.
 ;;
 ;; Redistribution and use in source and binary forms, with or without
 ;; modification, are permitted provided that the following conditions are
